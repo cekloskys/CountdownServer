a42dcbc3789e73135aa081b2dec61d6f971f2d64
steven20022 <35538211+steven20022@users.noreply.github.com>
2022-07-06 08:48:22 -0400

    Jacob Merge
    Added Cognitive Game UserAuth
    Added Louge Link UserAuth
    Removed id: Int from graphQL types
    Added _id: ID which uses the ObjectID from mongodb
    Changed how context is referenced

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 const { ApolloServer, gql } = require("apollo-server");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken")
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { DB_URI, DB_COUNTDOWN, COL_COURSEINFO, DB_GAMEDAY, COL_GAMEINFO, DB_LOGUELINK, COL_LINKINFO, COL_TUTORIALINFO } = process.env;
+const { 
+  DB_URI,
+  DB_COUNTDOWN,
+  COL_COURSEINFO,
+  DB_GAMEDAY,
+  COL_GAMEINFO,
+  COL_GAMEUSERS,
+  DB_LOGUELINK,
+  COL_LINKINFO,
+  COL_LINKUSERS,
+  COL_TUTORIALINFO,
+  JWT_SECRET 
+} = process.env;
+
+const getToken = (user) => jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+
+const getUserFromToken = async (token, db) => {
+  if (!token) { return null }
+
+  const tokenData = jwt.verify(token, JWT_SECRET);
+  //console.log(tokenData);
+  if (!tokenData?.id) {
+    return null;
+  }
+  const found = await db.find({ _id: ObjectId(tokenData.id) });
+
+  return found
+}
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -20,15 +49,16 @@ const typeDefs = gql`
   }
 
   type Game {
-    id: Int
+    _id: ID
     note: String
     image: String
     game: String
     solution: String
     title: String
-  } 
+  }
   
   type Link {
+    _id: ID
     id: String
     uri: String
     title: String
@@ -37,7 +67,47 @@ const typeDefs = gql`
    type Tutorial {
       id: String
       title: String
+      images: [String]
    } 
+
+    type Mutation {
+      signInGame(input: SignInInput!): AuthUser!
+      signInLink(id: String, pass: String): AuthUser!
+
+      createGame(note: String, game: String, solution: String, title: String): Boolean!
+      updateGame(id: Int!, updatedGame: GameInput!): Boolean!
+      deleteGame(id: ID): Boolean!
+
+      createLink(id: String, uri: String, title: String): Boolean!
+      updateLink(id: Int!, updatedGame: GameInput!): Boolean!
+      deleteLink(id: ID): Boolean!
+
+      createTutorial(id: String, title: String): Boolean!
+      updateTutorial(id: Int!, updatedGame: GameInput!): Boolean!
+      deleteTutorial(id: ID): Boolean!
+    }
+
+    input SignInInput {
+      id: String!
+      pass: String!
+    }
+
+    input GameInput {
+      note: String
+      game: String
+      solution: String
+      title: String
+    }
+
+    type AuthUser {
+      user: User!
+      token: String!
+    }
+
+    type User {
+      id: String!
+      name: String!
+    }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
@@ -52,6 +122,7 @@ const typeDefs = gql`
     courseByType(creditTypeCode: [String]): [Course]
 
     games: [Game]
+    gameSignedIn: Boolean
     
     links: [Link]
     tutorials: [Tutorial]
@@ -63,7 +134,7 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     courses: async (root, data, context) => {
-      return await context.col.find({}).toArray();
+      return await context.countdownCol.find({}).toArray();
     },
     coursesBy: async (root, data, context) => {
       console.log(data);
@@ -136,14 +207,141 @@ const resolvers = {
     courseByType: (root, data, context) => {
       return context.countdownCol.find({creditTypeCode: data.creditTypeCode}).toArray();
     },
-    games: (root, data, context) => {
+    games: async (root, data, context) => {
       return context.gameInfoCol.find({}).toArray();
+    },
+    gameSignedIn: (_, __, { user }) => {
+      if (user) return true;
+      return false
     },
     links: (root, data, context) => {
       return context.linkInfoCol.find({}).toArray();
     },
     tutorials: (root, data, context) => {
       return context.tutorialInfoCol.find({}).toArray();
+    },
+  },
+
+  Mutation: {
+    signInGame: async(_,{input}, context) => {
+      const user = await context.gameUsersCol.findOne({ id: input.id });
+
+      console.log(input);
+      
+      const isPasswordCorrect = user && await bcrypt.compare(input.pass, user.pass);
+
+      if (!user || !isPasswordCorrect) {
+        throw new Error('Invalid credentials!');
+      }
+
+      return {
+        user,
+        token: getToken(user),
+      }
+    },
+
+    createGame: async(_, data, { gameInfoCol, user }) => {
+     if (!user) { throw new Error('Authentication Error. Please sign in'); }
+     const note = data.note;
+     const game = data.game;
+     const solution = data.solution;
+     const title = data.title;
+      const newGameTemplate = {      
+        note,
+        game,
+        solution,
+        title
+      }
+      const result = await gameInfoCol.insertOne(newGameTemplate);
+      console.log(result.acknowledged);
+      return result.acknowledged// result.ops[0];
+    },
+
+    // updateGame: async(_, { id, updatedGame  }, { gameInfoCol, user }) => {
+    //   if (!user) { throw new Error('Authentication Error. Please sign in'); }
+
+    //   const result = await gameInfoCol.updateOne({id: id}, { updatedGame})
+    //   console.log(acknowledged);
+    //   return result.acknowledged;
+    // },
+
+    deleteGame: async(_, { id }, { gameInfoCol, user }) => {
+      if (!user) { throw new Error('Authentication Error. Please sign in'); }
+      
+      console.log(id);
+      const result = await gameInfoCol.deleteOne({ _id: ObjectId(id) });
+
+      console.log(result);
+
+      return result.acknowledged;
+    },
+
+    
+    signInLink: async(_,input, {linkUsersCol}) => {
+      const user = await linkUsersCol.findOne({ id: input.id });
+
+      console.log(input);
+      
+      const isPasswordCorrect = user && await bcrypt.compare(input.pass, user.pass);
+
+      if (!user || !isPasswordCorrect) {
+        throw new Error('Invalid credentials!');
+      }
+
+      return {
+        user,
+        token: getToken(user),
+      }
+    },
+
+    createLink: async(_, data, { linkInfoCol, user }) => {
+      if (!user) { throw new Error('Authentication Error. Please sign in'); }
+      const id = data.id;
+      const uri = data.uri;
+      const title = data.title;
+       const newLinkTemplate = {      
+         id,
+         uri,
+         title
+       }
+       const result = await linkInfoCol.insertOne(newLinkTemplate);
+       console.log(result.acknowledged);
+       return result.acknowledged// result.ops[0];
+     },
+
+    deleteLink: async(_, { id }, { linkInfoCol, user }) => {
+      if (!user) { throw new Error('Authentication Error. Please sign in'); }
+      
+      console.log(id);
+      const result = await linkInfoCol.deleteOne({ _id: ObjectId(id) });
+
+      console.log(result);
+
+      return result.acknowledged;
+    },
+
+    createTutorial: async(_, data, { tutorialInfoCol, user }) => {
+      if (!user) { throw new Error('Authentication Error. Please sign in'); }
+      const id = data.id;
+      const title = data.title;
+       const newTutorialTemplate = {      
+         id,
+         title
+       }
+       const result = await tutorialInfoCol.insertOne(newTutorialTemplate);
+       console.log(result.acknowledged);
+       return result.acknowledged// result.ops[0];
+     },
+
+    deleteTutorial: async(_, { id }, { tutorialInfoCol, user }) => {
+      if (!user) { throw new Error('Authentication Error. Please sign in'); }
+      
+      console.log(id);
+      const result = await tutorialInfoCol.deleteOne({ _id: ObjectId(id) });
+
+      console.log(result);
+
+      return result.acknowledged;
     },
   },
 };
@@ -154,25 +352,51 @@ const start = async () => {
     useUnifiedTopology: true,
   });
   await client.connect();
-  const db_countdown = client.db(DB_COUNTDOWN);
-  const countdownCol = db_countdown.collection(COL_COURSEINFO)
+  
+  const countdownCol = client.db(DB_COUNTDOWN).collection(COL_COURSEINFO);
 
-  const db_gameday = client.db(DB_GAMEDAY);
-  const gameInfoCol = db_gameday.collection(COL_GAMEINFO)
+  const gameInfoCol = client.db(DB_GAMEDAY).collection(COL_GAMEINFO);
+  const gameUsersCol = client.db(DB_GAMEDAY).collection(COL_GAMEUSERS);
 
-  const db_loguelink = client.db(DB_LOGUELINK);
-  const linkInfoCol = db_loguelink.collection(COL_LINKINFO)
-  const tutorialInfoCol = db_loguelink.collection(COL_TUTORIALINFO)
+  const linkInfoCol = client.db(DB_LOGUELINK).collection(COL_LINKINFO);
+  const tutorialInfoCol = client.db(DB_LOGUELINK).collection(COL_TUTORIALINFO);
+  const linkUsersCol = client.db(DB_LOGUELINK).collection(COL_LINKUSERS);
 
-  // console.log(db);
+  // const temp = await gameUsersCol.find({}).toArray();
+  // console.log(temp);
 
-  const context = {
-    countdownCol, gameInfoCol, linkInfoCol, tutorialInfoCol
-  };
+  
 
   // The ApolloServer constructor requires two parameters: your schema
   // definition and your set of resolvers.
-  const server = new ApolloServer({ typeDefs, resolvers, context });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+
+      };
+    },
+    context: async ({ req }) => {
+
+      const user = await getUserFromToken(req.headers.authorization, client.db(DB_GAMEDAY).collection(COL_GAMEUSERS) ) || '';
+     
+      
+      return{
+        user,
+
+        countdownCol,
+
+        gameInfoCol,
+        gameUsersCol,
+
+        linkInfoCol,
+        tutorialInfoCol,
+        linkUsersCol
+      }
+    }
+    
+  });
 
   // The `listen` method launches a web server.
   server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
